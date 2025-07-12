6a477da98c406ba5d6d78b00ac0811a059c09aa9
Yash Jaiswal <yashj180402@gmail.com>
2025-07-12 17:19:47 +0530

    Move Zeros To End

---
+++ Arrays/MoveZerosToEnd.java
@@ -0,0 +1,23 @@
+package Arrays;
+
+class MoveZerosToEnd {
+    static void pushZerosToEnd(int[] arr) {
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != 0) {
+                int temp = arr[i];
+                arr[i] = arr[count];
+                arr[count] = temp;
+                count++;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 0, 4, 3, 0, 5, 0};
+        pushZerosToEnd(arr);
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
